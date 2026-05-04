481226b56a76f3ae4a69dd8f359ca768e75486c7
nikhilrana13 <nikhilrajput060@gmail.com>
2026-05-04 18:17:55 +0530

    `Fixed typo in ProfileDropdown component: removed duplicate MdPerson icon`

--- user/src/components/common/ProfileDropdown.jsx
+++ user/src/components/common/ProfileDropdown.jsx
@@ -63,7 +63,7 @@ const ProfileDropdown = () => {
                 onClick={() => navigate("/profile")}
                 className="w-full flex items-center gap-3 px-4 py-3 hover:bg-gray-50 transition text-sm"
               >
-                <MdPerson className="text-lg text-gray-600" />
+                 <MdPerson className="text-lg text-gray-600" />
                 Profile
               </button>
 
